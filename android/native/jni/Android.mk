80381f4ad83a8c601cc4017c30b6d7e7741665f4
stang23 <shaofeng.tang@intel.com>
2016-03-31 15:13:31 +0800

    Add PIE flag for compiling in NDK

--- android/native/jni/Android.mk
+++ android/native/jni/Android.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../..
 LOCAL_MODULE := cgi-getfield
 LOCAL_SRC_FILES := ../../../cgi-getfield.c
@@ -8,6 +10,8 @@ include $(BUILD_EXECUTABLE)
 
 
 include $(CLEAR_VARS)
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../..
 LOCAL_MODULE := cgi-getcookie
 LOCAL_SRC_FILES := ../../../cgi-getcookie.c
@@ -15,6 +19,8 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -DUSE_WEBSOCKET
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../..
 LOCAL_MODULE := tinyweb
 LOCAL_SRC_FILES := ../../../websocket.c ../../../mongoose.c ../../../tinyweb.c
